2ede738a45af2ff6e9a8b9a6b918651d5c915dd8
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-08-20 21:51:10 +0500

    2.2

--- profile.js
+++ profile.js
@@ -97,3 +97,82 @@ function logOut() {
             console.log("Sign out error:", error);
         });
 }
+
+function editName() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            username = user.email;
+
+            {
+                db.collection("users")
+                    .get()
+                    .then((querySnapshot) => {
+                        querySnapshot.forEach(function(doc) {
+                            var data = doc.data();
+
+                            if (data.email === username) {
+                                // Display SweetAlert input for editing FirstName and LastName
+                                Swal.fire({
+                                    title: 'Edit Profile',
+                                    html: `<input id="swal-input-firstname" class="swal2-input" placeholder="First Name" value="${data.firstName || ''}">
+                           <input id="swal-input-lastname" class="swal2-input" placeholder="Last Name" value="${data.lastName || ''}">`,
+                                    focusConfirm: false,
+                                    showCancelButton: true,
+                                    preConfirm: () => {
+                                        const newFirstName = document.getElementById('swal-input-firstname').value;
+                                        const newLastName = document.getElementById('swal-input-lastname').value;
+
+                                        // Validate if inputs are not empty
+                                        if (!newFirstName.trim() || !newLastName.trim()) {
+                                            Swal.showValidationMessage('Please fill in both First Name and Last Name.');
+                                        } else {
+                                            // Update the values on the front end
+                                            data.firstName = newFirstName;
+                                            data.lastName = newLastName;
+                                        }
+                                    }
+                                }).then((result) => {
+                                    if (result.isConfirmed) {
+                                        // Update the Firestore document after confirming
+                                        db.collection("users").doc(doc.id).update({
+                                            firstName: data.firstName,
+                                            lastName: data.lastName
+                                        }).then(() => {
+                                            console.log("Profile updated successfully!");
+                                            Swal.fire({
+                                                icon: 'success',
+                                                title: 'Profile Updated',
+                                                showConfirmButton: false,
+                                                timer: 1500 // Show success message for 1.5 seconds
+                                            });
+                                            setTimeout(() => {
+                                                window.location.reload()
+                                            }, 0)
+                                        }).catch((error) => {
+                                            console.error("Error updating profile: ", error);
+                                            Swal.fire({
+                                                icon: 'error',
+                                                title: `Can't update`,
+                                                showConfirmButton: false,
+                                                timer: 1500 // Show success message for 1.5 seconds
+                                            });
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    })
+                    .catch((error) => {
+                        console.error("Error getting posts: ", error);
+                    });
+
+
+            }
+
+        } else {
+            window.location.href = "./login.html";
+            document.getElementById("headerName").innerText = "null";
+        }
+    });
+
+}
